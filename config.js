4d181c8ef703336919cb747694db65a5361c75e1
brayenwahidsuwiryo-commits <brayen.wahid.suwiryo@gmail.com>
2026-08-26 23:28:40 +0700

    Fix formatting of supabaseUrl in config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Isi dengan kredensial SUPABASE PROJECT Anda.
 // Gunakan ANON/PUBLISHABLE KEY saja. Jangan gunakan service_role key di browser.
 window.APP_CONFIG = {
-  supabaseUrl: "[https://qljartbxpirqoljakyql.supabase.co](https://qljartbxpirqoljakyql.supabase.co)",
+  supabaseUrl: "https://qljartbxpirqoljakyql.supabase.co",
   supabaseAnonKey: "sb_publishable_6uSG91UyXvTcbNPeaBxtbg_4tDQpXI3"
 };
